a2dc5ceb48b55b7e3799f84376a018d7b087d763
Gustavo <tavodlhoz9394@gmail.com>
2026-01-29 14:32:52 -0500

    solucion al pago1.7

---
+++ app/gracias/page.tsx
@@ -0,0 +1,8 @@
+export default function GraciasPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>¡Gracias por tu pago! 🙌</h1>
+      <p>Tu transacción fue procesada correctamente.</p>
+    </div>
+  );
+}
