124bc2ffe756d2d66f2a3bd2fef771bca5d43509
Gustavo <tavodlhoz9394@gmail.com>
2026-01-29 14:21:45 -0500

    solucion al pago1.5

---
+++ app/undefined/page.tsx
@@ -0,0 +1,8 @@
+export default function UndefinedPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Ruta /undefined alcanzada</h1>
+      <p>Si ves esto, Next está navegando aquí automáticamente.</p>
+    </div>
+  );
+}
