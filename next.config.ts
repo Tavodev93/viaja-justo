b489189739a8333910e5a97d684d7580e53f394b
Gustavo <tavodlhoz9394@gmail.com>
2026-02-03 22:38:19 -0500

    disable static export to enable api routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // NO output: "export"
 };
 
 export default nextConfig;
